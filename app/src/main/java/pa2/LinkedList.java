6fb27a80dc0797c6838be213a8d95cb48f586043
PoliticianSeal <politicianseal@gmail.com>
2024-09-18 14:06:03 -0400

    added test cases

--- app/src/main/java/pa2/LinkedList.java
+++ app/src/main/java/pa2/LinkedList.java
@@ -15,7 +15,7 @@ public class LinkedList {
      * Initialize the linked list with a null head
      */
     public LinkedList() {
-        
+        this.head = null;
     }
 
     /** 
@@ -25,7 +25,20 @@ public class LinkedList {
      * @return void
      */
     public void add(int value) {
+        Node node = new Node(value);
+        if (head == null || value < head.value) {
+            node.next = head;
+            head = node;
+            return;
+        }
 
+        Node current = head;
+        while (current.next != null && current.next.value <= value) {
+            current = current.next;
+        }
+
+        node.next = current.next;
+        current.next = node;
     }
 
     /**
@@ -35,16 +48,35 @@ public class LinkedList {
      * @return void
      */
     public void remove(int value) {
+        if (head == null) return;
 
+        if (head.value == value) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null && current.next.value != value) {
+            current = current.next;
+        }
+
+        if (current.next != null) {
+            current.next = current.next.next;
+        }
     }
-
     /**
      * Traverse the linked list RECURSIVELY and returns a string representation of the list
      * @param current
      * @return String
      */
     private String traverse(Node current){
-
+        if (current == null) {
+            return "";
+        }
+        if (current.next == null) {
+            return Integer.toString(current.value);
+        }
+        return Integer.toString(current.value) + " " + traverse(current.next);
     }
 
     /**
@@ -52,11 +84,10 @@ public class LinkedList {
      * @return String
      */
     public String traverse(){
-        
+        return traverse(this.head);
     }
-
     public String reverse(){
-        
+        return reverse(this.head);
     }
 
     /**
@@ -65,7 +96,13 @@ public class LinkedList {
      * @return String
      */
     private String reverse(Node current){
-        
+        if (current == null) {
+            return "";
+        }
+        if (current.next == null) {
+            return Integer.toString(current.value);
+        }
+        return(this.reverse(current.next) + " " + Integer.toString(current.value));
     }
     /**
      * Merges two sorted linked list and returns the 
@@ -74,8 +111,35 @@ public class LinkedList {
      * @param list2 The second sorted linked list 
      * @return LinkedLists
      */
+
+    // use two pointers to determine what you are going to add. curr1, curr2.
+
     public static LinkedList merge(LinkedList list1, LinkedList list2){
-       
+        LinkedList result = new LinkedList();
+        Node curr1 = list1.head;
+        Node curr2 = list2.head;
+
+        while (curr1 != null && curr2 != null) {
+            if (curr1.value <= curr2.value) {
+                result.add(curr1.value);
+                curr1 = curr1.next;
+            } else {
+                result.add(curr2.value);
+                curr2 = curr2.next;
+            }
+        }
+
+        while (curr1 != null) {
+            result.add(curr1.value);
+            curr1 = curr1.next;
+        }
+
+        while (curr2 != null) {
+            result.add(curr2.value);
+            curr2 = curr2.next;
+        }
+
+        return result;
     }
 
 
@@ -85,8 +149,19 @@ public class LinkedList {
         list.add(2);
         list.add(3);
         list.add(4);
-        System.out.println(list.traverse());
+        // System.out.println(list.reverse());
         list.remove(3);
-        System.out.println(list.traverse());
+        // System.out.println(list.traverse());
+
+        LinkedList list2 = new LinkedList();
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+        list2.add(5);
+        System.out.println("List1 reverse is " + list.reverse());
+        System.out.println("List1 is " + list.traverse());
+
+        LinkedList list3 = LinkedList.merge(list, list2);
+        System.out.println(list3.traverse(list3.head));
     }
 }
